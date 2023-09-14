ca5f7da4968ae504cd7c9b5b37c4f0c6c258533d
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-14 17:23:36 +0900

    Rubocop実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
     nickname              { 'test' }
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    email                 { Faker::Internet.email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     last_name             { '山田' }
     first_name            { '陸太郎' }
     last_name_kana        { 'ヤマダ' }
