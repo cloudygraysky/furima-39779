50b24eae292fed372f2a3111a03d248e45be79aa
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-14 18:20:41 +0900

    修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { 'test' }
     email                 { Faker::Internet.email }
-    password              { a00000 }
+    password              { 'a00000' }
     password_confirmation { password }
     last_name             { '山田' }
     first_name            { '陸太郎' }
