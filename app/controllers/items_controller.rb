cc2accb816a38b61e3a4d7d60737b6b97267e989
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-15 12:20:21 +0900

    ルーティング・コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,20 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+    redirect_to '/'
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :image).merge(user_id: current_user.id)
   end
 end
