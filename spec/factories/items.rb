3392b3aadc80a4f4c35d2ecaa665606e8d6c35a4
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-15 23:16:44 +0900

    テストコード設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name               { 'test' }
+    description        { 'testtesttest' }
+    price              { rand(300..9999999) }
+    category_id        { rand(2..11) }
+    condition_id       { rand(2..7) }
+    delivery_method_id { rand(2..3) }
+    delivery_date_id   { rand(2..4) }
+    prefecture_id      { rand(2..48) }
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+    association :user
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,92 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品の保存' do
+    context '商品出品ができる場合' do
+      it 'すべての項目を入力すれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品ができない場合' do
+      it '画像が空では登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では出品できない' do
+        @item.name  = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品名が40文字以上は入力できない' do
+        @item.name = 'a' * 41
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name is too long (maximum is 40 characters)")
+      end
+      it '商品の説明が空では出品できない' do
+        @item.description  = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it '商品の説明が1000文字以上は入力できない' do
+        @item.description = 'a' * 1001
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description is too long (maximum is 1000 characters)")
+      end
+      it 'カテゴリーが空では出品できない' do
+        @item.category_id  = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態が空では出品できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担が空では出品できない' do
+        @item.delivery_method_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery method can't be blank")
+      end
+      it '発送元の地域が空では出品できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数が空では出品できない' do
+        @item.delivery_date_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery date can't be blank")
+      end
+      it '価格が空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it '価格は整数でないと出品できない' do
+        @item.price = '0.1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
+      it '価格は半角でないと出品できない' do
+        @item.price = '１１１１'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '価格は300円未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be in 300..9999999")
+      end
+      it '価格は1000万円以上では出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be in 300..9999999")
+      end
+    end
+  end
 end
