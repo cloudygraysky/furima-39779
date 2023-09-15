130ab11cdf310662c9e8f7321bb532c669ea9d11
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-15 11:00:23 +0900

    アソシエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_method
+  belongs_to :delivery_date
+  belongs_to :prefecture
+
   belongs_to :user
   has_one_attached :image
 end
