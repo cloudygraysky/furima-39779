0fe1b31552eb0592a26cb7caf1691a9c0834bba6
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-15 11:16:34 +0900

    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,9 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :name, :description, :price, :user presence: true
+
+  validates :category_id, :condition_id, :delivery_method_id, :delivery_date_id, :prefecture_id numericality: { other_than: 1 , message: "can't be blank"}
+
 end
