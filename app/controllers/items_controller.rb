8b8a483bbc2beb26162ee0b1332b20f84a1a39cd
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-18 14:07:53 +0900

    コメントアウト(未設定のため)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,11 +22,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def edit
-  end
+  #def edit
+  #end
 
-  def destroy
-  end
+  #def destroy
+  #end
 
   private
 
