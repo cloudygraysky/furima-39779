4c8da76397316379fef226112371df4385a4c131
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-18 12:38:06 +0900

    Rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = Item.all.order("created_at DESC")
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
@@ -27,6 +27,7 @@ class ItemsController < ApplicationController
 
   def destroy
   end
+
   private
 
   def item_params
