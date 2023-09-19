803d49838b8f874f2f9a8a861ac8f2f6d949749a
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-19 14:08:57 +0900

    コントローラーユーザーログイン修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, except: [:index, :new, :create]
-
   before_action :move_to_index, only: [:edit, :update]
 
   def index
