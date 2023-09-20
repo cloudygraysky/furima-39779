0cb08e69a0a4eeb5291d3a8da442522891acf87f
cloudygraysky <koishikawa_2003@yahoo.co.jp>
2023-09-20 19:40:05 +0900

    入力・表示画面設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,25 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
 
   def index
+    @order_address = OrderAddress.new
+    @item = Item.find_by(id: params[:item_id])
   end
 
+  def create
+    @item = Item.find_by(id: params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id)
+  end
 end
